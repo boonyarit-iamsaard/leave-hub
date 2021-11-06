0c92c8599c375950ce4d4628e3d0a32317e98e99
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-11-06 17:21:03 +0700

    build: add material & date-fns

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import { FC } from 'react';
 
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import { Button, Stack } from '@mui/material';
+
 const App: FC = () => {
   return (
     <div>
       <p>Bangkok Engineering - Leave Hub</p>
+
+      <Stack direction="row" spacing={2} sx={{ p: 2 }}>
+        <Button variant="outlined" startIcon={<DeleteIcon />}>
+          Delete
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />}>
+          Send
+        </Button>
+      </Stack>
     </div>
   );
 };
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#006564',
+    },
+  },
+  shape: {
+    borderRadius: 10,
+  },
+  typography: {
+    fontFamily: '"Inter", sans-serif',
+  },
+});
