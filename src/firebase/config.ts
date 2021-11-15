d1b1d282b69acd32a5e2913c143957a3e68f4476
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-11-15 09:18:54 +0700

    build: update firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,5 +15,6 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 
 const auth = getAuth();
+const database = getDatabase();
 
-export { auth };
+export { auth, database };
