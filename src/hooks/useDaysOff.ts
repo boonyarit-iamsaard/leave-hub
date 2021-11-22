16f37c2c6b1cf8013b47b131fe5f6a12ec7e6bcf
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-11-22 10:40:32 +0700

    fix: realtime database performance issue

--- src/hooks/useDaysOff.ts
+++ src/hooks/useDaysOff.ts
@@ -1,27 +1,28 @@
 import { useEffect, useState } from 'react';
 
 import { realtimeDatabase } from '../firebase/config';
-import {
-  ref,
-  onChildAdded,
-  onChildChanged,
-  onChildRemoved,
-  onValue,
-} from 'firebase/database';
+import { ref, onValue } from 'firebase/database';
 
 // interfaces
-import { Shift } from '../interfaces/roster.interface';
+import { Roster, Shift } from '../interfaces/roster.interface';
 
-const useDaysOff = (): { daysOff: Shift[] } => {
+const useDaysOff = (): {
+  daysOff: Shift[];
+  findDayOff: (roster: Roster) => Shift | undefined;
+} => {
   const [daysOff, setDaysOff] = useState<Shift[]>([]);
 
+  const findDayOff = (roster: Roster) => {
+    return daysOff.find(dayOff => dayOff.id === roster.shiftId);
+  };
+
   useEffect(() => {
-    const daysOff: Shift[] = [];
     const daysOffRef = ref(realtimeDatabase, 'days-off');
 
     const daysOffListener = onValue(
       daysOffRef,
       snapshot => {
+        const daysOff: Shift[] = [];
         snapshot.forEach(childSnapshot => {
           const dayOff: Shift = {
             ...childSnapshot.val(),
@@ -41,50 +42,12 @@ const useDaysOff = (): { daysOff: Shift[] } => {
       }
     );
 
-    const onDayOffAddedListener = onChildAdded(daysOffRef, dayOff => {
-      const dayOffData: Shift = {
-        ...dayOff.val(),
-        id: dayOff.key,
-        // transform the date from firebase to a date object
-        startDate: new Date(dayOff.val().startDate),
-        endDate: new Date(dayOff.val().endDate),
-      };
-
-      // setDaysOff([...daysOff, dayOffData]);
-      setDaysOff(prevState => prevState.concat(dayOffData));
-    });
-
-    const onDayOffChangedListener = onChildChanged(daysOffRef, dayOff => {
-      const dayOffIndex = daysOff.findIndex(({ id }) => id === dayOff.key);
-
-      const dayOffData: Shift = {
-        ...dayOff.val(),
-        id: dayOff.key,
-        // transform the date from firebase to a date object
-        startDate: new Date(dayOff.val().startDate),
-        endDate: new Date(dayOff.val().endDate),
-      };
-
-      setDaysOff(prevState => {
-        const newState = [...prevState];
-        newState[dayOffIndex] = dayOffData;
-        return newState;
-      });
-    });
-
-    const onDayOffRemovedListener = onChildRemoved(daysOffRef, dayOff => {
-      setDaysOff(prevState => prevState.filter(({ id }) => id !== dayOff.key));
-    });
-
     return () => {
       daysOffListener();
-      onDayOffAddedListener();
-      onDayOffChangedListener();
-      onDayOffRemovedListener();
     };
   }, []);
 
-  return { daysOff };
+  return { daysOff, findDayOff };
 };
 
 export default useDaysOff;
