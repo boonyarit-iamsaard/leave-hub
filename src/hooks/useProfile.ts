f9102a777c090a25256aded8bb7374cb7e0960a1
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-11-29 14:39:02 +0700

    refactor: get profile from database instead of user list

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,27 +1,34 @@
 import { useEffect, useState } from 'react';
 
+import { doc, onSnapshot } from '@firebase/firestore';
+import { firestoreDatabase } from '../firebase/config';
+
 // interfaces
 import { Profile } from '../interfaces/auth.interface';
 
 // hooks
-import useUserList from './useUserList';
 import useAuthContext from './useAuthContext';
 
 const useProfile = (): { profile: Profile } => {
-  const { userList } = useUserList();
   const {
     state: { user },
   } = useAuthContext();
   const [profile, setProfile] = useState<Profile>({} as Profile);
 
   useEffect(() => {
-    if (user && user.uid) {
-      const { uid } = user;
-      const profile = userList.find(user => user.uid === uid);
-
-      if (profile) setProfile(profile);
+    if (user) {
+      const profileRef = doc(firestoreDatabase, 'users', user.uid);
+      const unsubscribeProfile = onSnapshot(profileRef, profileSnapshot => {
+        if (profileSnapshot.exists()) {
+          const profileData = profileSnapshot.data();
+          setProfile({
+            ...profileData,
+          } as Profile);
+        }
+      });
+      return () => unsubscribeProfile();
     }
-  }, [userList, user]);
+  }, [user]);
 
   return { profile };
 };
